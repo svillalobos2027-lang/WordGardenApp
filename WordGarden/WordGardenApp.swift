f6e148c5ffca878320800a535c5c176ba5bad519
Stephanie Villalobos <svillalobos2027@gmail.com>
2026-01-12 10:54:04 -0800

    Initial Commit

---
+++ WordGarden/WordGardenApp.swift
@@ -0,0 +1,17 @@
+//
+//  WordGardenApp.swift
+//  WordGarden
+//
+//  Created by VILLALOBOS, STEPHANIE on 1/12/26.
+//
+
+import SwiftUI
+
+@main
+struct WordGardenApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
