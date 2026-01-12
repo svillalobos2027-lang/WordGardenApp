0c84ecd1104c76b9255fb9d8c35a7ef4a22246f9
Stephanie Villalobos <svillalobos2027@gmail.com>
2026-01-12 11:49:34 -0800

    first day

--- WordGarden/ContentView.swift
+++ WordGarden/ContentView.swift
@@ -8,12 +8,54 @@
 import SwiftUI
 
 struct ContentView: View {
+    @State private var wordsGuessed = 0
+    @State private var wordsMissed = 0
+    @State private var wordsToGuess = ["SWIFT", "DOG", "CAT"] //ALL CAPS
+    @State private var gameStatusMessage = "How Many Guesses to Uncover the Hidden Word?"
+    @State private var currentWord = 0
+    @State private var guessedLetter = ""
+    
     var body: some View {
         VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+            HStack{
+                VStack(alignment: .leading){
+                    Text("Words Guessed: \(wordsGuessed)")
+                    Text("Words Missed: \(wordsMissed)")
+                }
+                Spacer()
+                VStack(alignment: .trailing){
+                    Text("Words to Guess: \(wordsToGuess.count - (wordsGuessed + wordsMissed))")
+                    Text("Words in Game: \(wordsToGuess.count)")
+                }
+            }
+            Spacer()
+            Text(gameStatusMessage)
+                .font(.title)
+                .multilineTextAlignment(.center)
+            
+            //TODO: Switch to wordsToGuess[currentWord]
+            Text("_ _ _ _ _")
+                .font(.title)
+            
+            
+            
+            HStack{
+                TextField("", text: $guessedLetter)
+                    .textFieldStyle(.roundedBorder)
+                    .frame(width: 30)
+                    .overlay {
+                        RoundedRectangle(cornerRadius: 5)
+                            .stroke(.gray, lineWidth: 2)
+                    }
+                
+                Button("Guess a Letter:") {
+                    //TODO: Guess a Letter button action here
+                }
+                .buttonStyle(.bordered)
+                .tint(.mint)
+            }
+
+            Spacer()
         }
         .padding()
     }
